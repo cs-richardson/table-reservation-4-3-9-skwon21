ca03e66b7d5c086ce6e864b774eefd70a41fe5ae
San Kwon <skwon21@ssis.edu.vn>
2019-08-25 11:44:20 +0700

    Create Table Reservation (4.3.9) .py

---
+++ Table Reservation (4.3.9) .py
@@ -0,0 +1,7 @@
+# codehs 4.3.9 Table Reservation
+user_name = input("Name: ")
+reservation_name = "Shonda"
+if user_name == reservation_name:
+    print ("Right this way!")
+else: 
+    print ("Sorry, we don't have a reservation under that name.")
